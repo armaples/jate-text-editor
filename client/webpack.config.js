4e85c1e1ff0d278bfcb033c8510260b8c7463e5d
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-17 12:34:17 -0400

    Added plugins to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text Editor',
+        start_url: './',
+        publicPath: './',
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new MiniCssExtractPlugin(),
     ],
 
     module: {
