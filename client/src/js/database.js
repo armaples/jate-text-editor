d9e3af134c1ee685c45e5b94bbc51a56cd9f27b3
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-17 12:49:17 -0400

    Set up method to retrieve data from indexedDB in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,6 +23,15 @@ export const putDb = async (content) => {
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Retrieving data from the database...');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('Retrieved data from database!', result);
+  return result.content;
+}
 
 initdb();
