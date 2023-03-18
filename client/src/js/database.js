a2a4d30e78b79a8849211cc366a72d851b13d56b
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-17 20:14:26 -0400

    Added comments to database file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// initializes the indexedDB
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,11 +13,16 @@ const initdb = async () =>
     },
   });
 
+// Updates the indexedDB database with text
 export const putDb = async (content) => {
   console.log('Updating the database...');
+  // creates a connection to database and the version of that database to be used
   const jateDB = await openDB('jate', 1);
+  // creates a new "transaction" and specifies the database and permissions (can read and edit)
   const tx = jateDB.transaction('jate', 'readwrite');
+  // opens the objectStore which allows us to make changes to database
   const store = tx.objectStore('jate');
+  // uses .put to store content with associated id 
   const request = store.put({ id: 1, content: content });
   const result = await request;
   console.log('Database successfully updated!', result);
@@ -24,9 +30,13 @@ export const putDb = async (content) => {
 
 export const getDb = async () => {
   console.log('Retrieving data from the database...');
+    // creates a connection to database and the version of that database to be used
   const jateDb = await openDB('jate', 1);
+    // creates a new "transaction" and specifies the database and permissions (can only read not edit)
   const tx = jateDb.transaction('jate', 'readonly');
+    // opens the objectStore which allows us to make changes to database
   const store = tx.objectStore('jate');
+  // uses .get to get the most recently saved ver of the text 
   const request = store.get(1);
   const result = await request;
   console.log('Retrieved data from database!', result);
